c8d179b1991c00b34d2d4360dbb1e3c19dec3ff4
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-11-18 23:24:46 +0530

    React: Components created for Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavbarComponent from "./components/NavbarComponent";
-import Photos from "./components/Photos";
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavbarComponent from "./components/NavbarComponent"
+import Photos from "./components/Photos"
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
 
   return (
     <>
       <NavbarComponent />
-			<Photos />
+			{/* <Photos /> */}
+			{/* <About /> */}
+			<Contact />
     </>
   );
 }
