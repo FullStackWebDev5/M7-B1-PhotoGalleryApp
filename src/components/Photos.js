1e5cb9a965107bbe63aadf6a00476852f02b77bf
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-11-18 23:04:36 +0530

    PhotoApp: API Integration complete

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,95 +1,52 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Card, Spinner } from "react-bootstrap";
+import axios from "axios";
 
 const Photos = () => {
-  let photosArray = [
-    {
-      id: "0",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/yC-Yzbqy7PY",
-      download_url: "https://picsum.photos/id/0/5000/3333",
-    },
-    {
-      id: "1",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/LNRyGwIJr5c",
-      download_url: "https://picsum.photos/id/1/5000/3333",
-    },
-    {
-      id: "26",
-      author: "Vadim Sherbakov",
-      width: 4209,
-      height: 2769,
-      url: "https://unsplash.com/photos/tCICLJ5ktBE",
-      download_url: "https://picsum.photos/id/26/4209/2769",
-    },
-    {
-      id: "27",
-      author: "Yoni Kaplan-Nadel",
-      width: 3264,
-      height: 1836,
-      url: "https://unsplash.com/photos/iJnZwLBOB1I",
-      download_url: "https://picsum.photos/id/27/3264/1836",
-    },
-    {
-      id: "28",
-      author: "Jerry Adney",
-      width: 4928,
-      height: 3264,
-      url: "https://unsplash.com/photos/_WiFMBRT7Aw",
-      download_url: "https://picsum.photos/id/28/4928/3264",
-    },
-    {
-      id: "29",
-      author: "Go Wild",
-      width: 4000,
-      height: 2670,
-      url: "https://unsplash.com/photos/V0yAek6BgGk",
-      download_url: "https://picsum.photos/id/29/4000/2670",
-    },
-    {
-      id: "2",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/N7XodRrbzS0",
-      download_url: "https://picsum.photos/id/2/5000/3333",
-    },
-    {
-      id: "3",
-      author: "Alejandro Escamilla",
-      width: 5000,
-      height: 3333,
-      url: "https://unsplash.com/photos/Dl6jeyfihLk",
-      download_url: "https://picsum.photos/id/3/5000/3333",
-    },
-  ];
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://picsum.photos/v2/list")
+      .then((res) => setPhotos(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Container>
       <Row>
-        {photosArray.map((photo) => (
-          <Col md={3} className="mt-2 mb-2" key={photo.id}>
-            <Card style={{ width: "18rem", textAlign: "center" }}>
-              <Card.Img
-                variant="top"
-                src={photo.download_url}
-                style={{ height: "200px" }}
-              />
-              <Card.Body>
-                <Card.Title>{photo.author}</Card.Title>
-                <a href={photo.url} className="btn btn-dark btn-sm">
-                  View details
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        {photos.length ? (
+          photos.map((photo) => (
+            <Col md={3} className="mt-2 mb-2" key={photo.id}>
+              <Card style={{ width: "18rem", textAlign: "center" }}>
+                <Card.Img
+                  variant="top"
+                  src={photo.download_url}
+                  style={{ height: "200px" }}
+                />
+                <Card.Body>
+                  <Card.Title>{photo.author}</Card.Title>
+                  <a href={photo.url} className="btn btn-dark btn-sm">
+                    View details
+                  </a>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <Spinner
+            animation="border"
+            role="status"
+            style={{ margin: "200px auto" }}
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        )}
       </Row>
     </Container>
   );
 };
 
 export default Photos;
+
+// Various types of request method/ HTTP methods - GET, POST, etc
